d3bd103bee28c6e8dfe7bbc71c28c6f2bc2f89ef
Mo Lin <molin@wanchain.org>
2019-08-26 14:57:49 +0800

    update testnet

---
+++ monitor-testnet.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#set -x
+
+echo "Testnet blockNumber monitor start"
+blockNumber="N/A"
+oldBlockNumber="N/A"
+err=0
+while true 
+do
+blockNumber=$(curl -X POST --data '{"method":"eth_blockNumber","params":[],"id":"1","jsonrpc":"2.0"}' http://localhost:3333/)
+echo $blockNumber
+
+if [ "$blockNumber" == "$oldBlockNumber" ]
+then 
+    ((err++))
+else
+    err=0
+fi
+
+if [ "$err" -gt 2 ] 
+then
+    echo $blockNumber|mail -v -s "Testnet blockNumber stoped" molin@wanchain.org
+    err=0
+fi
+
+sleep 5
+oldBlockNumber=$blockNumber
+echo $oldBlockNumber
+done
+
+
+
